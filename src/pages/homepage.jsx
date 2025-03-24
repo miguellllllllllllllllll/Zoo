acd1248a0eaabf78b49f5c5699d0203ba0084dbf
Nikolai <na.koenig@outlook.com>
2025-03-24 16:39:55 +0100

    Updated Navbar name

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,5 +1,5 @@
 import "../App.css";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import { Button } from "@mui/material";
 import { Box } from "@mui/material";
 
