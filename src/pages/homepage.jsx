0346d81008cf6c337cf20c6e36e5f0912dfd1e30
Nikolai <na.koenig@outlook.com>
2025-03-30 19:12:41 +0200

    Fixed imports

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,6 +2,7 @@ import "../App.css";
 import Navbar from "../components/Navbar";
 import { Button } from "@mui/material";
 import { Box } from "@mui/material";
+import Main_Picture from "../assets/Homepage_Picture.jpg";
 
 function Homepage() {
   return (
@@ -10,7 +11,14 @@ function Homepage() {
         <Navbar />
       </header>
       <main>
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <img src="{$Main_Picture}" alt="" />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            margin: "calc(100vh - 10vh) 0 0 0",
+          }}
+        >
           <Button variant="contained">Tickets bestellen</Button>
         </Box>
       </main>
