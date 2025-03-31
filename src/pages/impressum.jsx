22cca7591de9edca17c86a8ae87d07bc59b3b64c
Sven Toye <cool_cucumber_cat@outlook.com>
2025-03-31 16:47:42 +0200

    outline

---
+++ src/pages/impressum.jsx
@@ -0,0 +1,22 @@
+import "../App.css";
+import { Box } from "@mui/material";
+
+export default function () {
+	return (
+		<Box>
+			<h1>Kontakt</h1>
+			<p>Zoo Zürich AG</p>
+			<p>Zürichbergstrasse 221</p>
+			<p>8044 Zürich</p>
+			<p>
+				<a href="+41442542500">044 254 25 00</a>
+			</p>
+			<p>
+				<a href="zoo@zoo.ch">zoo@zoo.ch</a>
+			</p>
+			<h1>Handelsregistereintrag</h1>
+			<p>Eingetragener Firmenname: Zoo Zürich AG</p>
+			<p>Nummer: CHE-101.907.073</p>
+		</Box>
+	);
+}
